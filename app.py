79d1b7892e48966f3cea3dcaedf91c0670d96228
Anshuman Pandey <a.pandeyj28@gmail.com>
2026-07-18 00:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,7 @@ demo = gr.Interface(
 )
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    demo.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 7860))
+)
