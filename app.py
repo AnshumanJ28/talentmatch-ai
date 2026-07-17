eecd77d0909fd706ee8b797536924d0f239b78bd
Anshuman Pandey <a.pandeyj28@gmail.com>
2026-07-18 00:56:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import gradio as gr
-
 from pathlib import Path
 
 from src.pipeline import InferencePipeline
@@ -29,7 +28,4 @@ demo = gr.Interface(
 )
 
 if __name__ == "__main__":
-    demo.launch(
-    server_name="0.0.0.0",
-    server_port=int(os.environ.get("PORT", 7860))
-)
+    demo.launch(server_name="0.0.0.0", server_port=7860)
